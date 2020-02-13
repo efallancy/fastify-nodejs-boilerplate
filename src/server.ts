31544d2ad47059f0118f98d47caebda35101da61
Emmanuel Fallancy <emmanuel.fallancy@gmail.com>
2020-02-14 00:53:08 +1100

    Registering middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import fastify, { FastifyInstance } from 'fastify';
+import cors from 'fastify-cors';
+import helmet from 'fastify-helmet';
 
 import config from './config';
 import health from './routes/health';
 
 export const configureServer = (): FastifyInstance => {
   const server = fastify({ logger: config.logger, genReqId: config.genReqId });
+
+  // Register middleware
+  server.register(cors);
+  server.register(helmet);
 
   // Register/mount route(s)
   server.register(health);
